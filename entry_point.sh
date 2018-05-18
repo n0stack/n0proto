dcd59d33b16188bc533e6bde6f107db16814a0cf
h-otter <h-otter@outlook.jp>
2018-05-19 03:50:46 +0900

    fix entry_point to use multiple proto files in one directory

--- entry_point.sh
+++ entry_point.sh
@@ -5,7 +5,9 @@ dirs=`find /src -type d | grep -v .git | grep -v test`
 
 for d in $dirs
 do
-  if [ -e $d/model.proto ]; then
+  # 複数のファイルを指定できない
+  ls -1 $d/*.proto > /dev/null 2>&1
+  if [ "$?" = "0" ]; then
     protoc -I/usr/local/include -I/src -I/go/src -I/go/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis $* $d/*.proto
   fi
 done
